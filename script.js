8269b7ceab26d01b1e5f680d416bb10551a6468d
Christopher Clair <124112751+cwclair@users.noreply.github.com>
2023-05-10 17:18:56 -0500

    getMovieInfo: trims event target text adds plus signs in place of spaces

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // OMDb key is 333546da
 
+// Create constant arrays of drinks (gathered via Cocktail Database api) for each genre? Perhaps 3 to 5 options per array?
 var movieInputEl = document.querySelector('#movieInput');
 var movieForm = document.querySelector('#movieForm');
 var resultsDiv = document.querySelector("#results")
@@ -50,6 +51,29 @@ function addMovieTitle(movies){
   }
   function getMovieInfo(event){
     console.log(event.target)
+    
+    // Trims the year from the event target's text content
+    var movieInfo = event.target.textContent;
+    console.log(movieInfo);
+    var index = movieInfo.lastIndexOf(' ');
+    movieInfo = movieInfo.substring(0, index);
+    // Puts a "+" in place of any spaces in the title, for API URL purposes
+    var movieInfoPlus = movieInfo.replaceAll(' ', '+');
+    console.log(movieInfoPlus);
+
+    /* CHRIS ASKS: AFTER THE TRIM ABOVE, DOES THIS SEQUENCE MAKE SENSE?:
+    
+    Create API using movieInfoPlus variable:
+    https://www.omdbapi.com/?apikey=333546da&t=${movieInfoPlus}
+    
+    Fetch complete movie info using said API URL
+
+    Use genre info from selected film, randomly select a drink from the drink category that we tie to said genre. Perhaps we have to create an array of a few drinks for each category?
+
+    Display movie title with poster image on page, with drink pairing (and drink thumbnail image?) alongside/underneath
+    
+    
+    */
 
 
 
